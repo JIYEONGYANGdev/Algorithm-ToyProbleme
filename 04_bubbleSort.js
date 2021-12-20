a7cdfb0bab5a63945dc5da5bf2f367b6d8f50974
JIYEONGYANGdev <sootulliyang.dev@gmail.com>
2021-12-21 01:22:20 +0900

    bubble sort to be ascending

---
+++ 04_bubbleSort.js
@@ -0,0 +1,75 @@
+// parameter 정수 배열
+// RETURN 오름차순 정렬 
+/* 
+* bubble sort!
+1. 첫번째 요소가 두번째 요소보다 크면 두 요소 위치 swap!
+2. 두번째-세번째 비교 swap!
+3. 1~2를 반복(마지막 직전-마지막 요소 비교)
+4. 3까지 한번 거치면 가장 큰 정수요소가 배열 마지막으로 정렬됨
+5. 1~3을 반복 => 두번째로 큰 요소가 배열 마지막 직전 정렬
+n(배열 길이)만큼 반복 
+*/
+
+const swap = function (idx1, idx2, arr) {
+  // 두 변수를 바꾸는 방법
+
+  // 1) 임시 변수를 활용한 방법
+  // let temp = arr[idx1];
+  // arr[idx1] = arr[idx2];
+  // arr[idx2] = temp;
+
+  // 2) Destructuring assignment를 활용한 방법
+  // arr이 reference type이라 가능
+  [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+
+  // 3) XOR 연산을 활용한 방법
+  // arr이 reference type이라 가능
+  // arr[idx1] ^= arr[idx2];
+  // arr[idx2] ^= arr[idx1];
+  // arr[idx1] ^= arr[idx2];
+};
+
+// naive solution
+// let bubbleSort = function (arr) {
+//   let N = arr.length;
+
+//   for (let i = 0; i < N - 1; i++) {
+//     // 매 반복(iteration)마다 i번째로 큰 수가 마지막에서 i번째 위치하게 된다.
+//     // 이미 정렬된 요소는 고려할 필요가 없으므로, 'j < N - 1 - i'만 비교하면 된다.
+//     for (let j = 0; j < N - 1 - i; j++) {
+//       if (arr[j] > arr[j + 1]) {
+//         swap(j, j + 1, arr);
+//       }
+//     }
+//   }
+
+//   return arr;
+// };
+
+// optimized solution
+let bubbleSort = function (arr) {
+  let N = arr.length;
+
+  for (let i = 0; i < N; i++) {
+    // swap 횟수를 기록한다.
+    // 어떤 요소도 swap되지 않은 경우, 배열은 정렬된 상태이다.
+    let swaps = 0;
+
+    // 매 반복(iteration)마다 i번째로 큰 수가 마지막에서 i번째 위치하게 된다.
+    // 이미 정렬된 요소는 고려할 필요가 없으므로, 'j < N - 1 - i'만 비교하면 된다.
+    for (let j = 0; j < N - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        swaps++;
+        swap(j, j + 1, arr);
+      }
+    }
+
+    if (swaps === 0) {
+      break;
+    }
+  }
+
+  return arr;
+};
+
+console.log(bubbleSort([2,1,3,0]))
